8d4cf9bdb18adb657de0183d18a419cd6e6ad57a
chayan200 <krishnendu1103.be21@chitkarauniversity.edu.in>
2024-02-09 15:17:19 +0530

    Quizapp

---
+++ Assessment/data/common-knowledge.js
@@ -0,0 +1,209 @@
+const quizData = {
+  title: "Quize app Application"
+};
+
+const questions = [
+  {
+    text: "What is the capital of France?",
+    type: "mc",
+    answers: [
+      { text: "Berlin", correct: true },
+      { text: "Rome", correct: false },
+      { text: "Paris", correct: false},
+      { text: "Madrid", correct: false }
+    ]
+  },
+  {
+    text: "Who wrote the play Hamlet?",
+    type: "mc",
+    answers: [
+      { text: "William Shakespeare", correct: true },
+      { text: "George Bernard Shaw", correct: false },
+      { text: "Oscar Wilde", correct: false},
+      { text: "Charles Dickens", correct: false }
+    ]
+  },
+  {
+    text: "What is the chemical symbol for water?",
+    type: "mc",
+    answers: [
+      { text: "H2o", correct: true },
+      { text: "Co2", correct: false },
+      { text: "Nacl", correct: false},
+      { text: "O2", correct: false }
+    ]
+  },
+  {
+    text: "Which planet is known as the Red Planet?",
+    type: "mc",
+    answers: [
+      { text: "Venus", correct: false },
+      { text: "Marse", correct: true },
+      { text: "Jupiter", correct: false},
+      { text: "Satan", correct: false }
+    ]
+  },
+  {
+    text: "Who painted the Mona Lisa?",
+    type: "mc",
+    answers: [
+      { text: "Vincent van Gogh", correct: true },
+      { text: "Pablo Picasso", correct: false },
+      { text: "Leonardo da Vinci", correct: false},
+      { text: "Michelangelo", correct: false }
+    ]
+  },
+  {
+    text: "Which country is famous for the Taj Mahal?",
+    type: "mc",
+    answers: [
+      { text: "India", correct: true },
+      { text: "China", correct: false },
+      { text: "Pakistan", correct: false},
+      { text: "Brazil", correct: false }
+    ]
+  },
+  {
+    text: "What is the capital city of Japan?",
+    type: "mc",
+    answers: [
+      { text: "Beijing", correct: true },
+      { text: "Seoul", correct: false },
+      { text: "Tokyo", correct: false},
+      { text: "Bangkok", correct: false }
+    ]
+  },
+  {
+    text: "What is the symbol for the chemical element iron?",
+    type: "mc",
+    answers: [
+      { text: "Fe", correct: true },
+      { text: "I", correct: false },
+      { text: "ir", correct: false},
+      { text: "In", correct: false }
+    ]
+  },
+  {
+    text: "Who wrote the novel To Kill a Mockingbird?",
+    type: "mc",
+    answers: [
+      { text: "J.K. Rowling", correct: true },
+      { text: "Harper Lee", correct: false },
+      { text: "Ernest Hemingway", correct: false},
+      { text: "Mark Twain", correct: false }
+    ]
+  },
+  {
+    text: "Which ocean is the largest?",
+    type: "mc",
+    answers: [
+      { text: "Pacific Ocean", correct: true },
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Indian Ocean", correct: false},
+      { text: "Arctic Ocean", correct: false }
+    ]
+  },
+  {
+    text: "What is the capital of Australia?",
+    type: "mc",
+    answers: [
+      { text: "Sydney", correct: true },
+      { text: "Canberra", correct: false },
+      { text: "Melbourne", correct: false},
+      { text: "Perth", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Isaac Newton", correct: true },
+      { text: "Albert Einstein", correct: false },
+      { text: "Galileo Galilei", correct: false},
+      { text: "Charles Darwin", correct: false }
+    ]
+  },
+  {
+    text: "Which instrument is used to measure atmospheric pressure?",
+    type: "mc",
+    answers: [
+      { text: "Thermometer", correct: true },
+      { text: "Barometer", correct: false },
+      { text: "Hygrometer", correct: false},
+      { text: "Altimeter", correct: false }
+    ]
+  },
+  {
+    text: "What is the currency of Canada?",
+    type: "mc",
+    answers: [
+      { text: "Euro", correct: true },
+      { text: "Dollar", correct: false },
+      { text: "Peso", correct: false},
+      { text: "Pound", correct: false }
+    ]
+  },
+  {
+    text: "Who is known as the father of modern physics?",
+    type: "mc",
+    answers: [
+      { text: "sIsaac Newton", correct: true },
+      { text: "Albert Einstein", correct: false },
+      { text: "Galileo Galilei", correct: false},
+      { text: "Nikola Tesla", correct: false }
+    ]
+  },
+  {
+    text: "What is the tallest mountain in the world?",
+    type: "mc",
+    answers: [
+      { text: "Mount Everest", correct: true },
+      { text: "K2", correct: false },
+      { text: "Mount Kilimanjaro", correct: false},
+      { text: "Mount Fuji", correct: false }
+    ]
+  },
+  {
+    text: "Which gas makes up the majority of Earth's atmosphere?",
+    type: "mc",
+    answers: [
+      { text: "Oxygen", correct: true },
+      { text: "Nitrogen", correct: false },
+      { text: "Carbon Dioxide", correct: false},
+      { text: "Helium", correct: false }
+    ]
+  },
+  {
+    text: "Who wrote Romeo and Juliet?",
+    type: "mc",
+    answers: [
+      { text: "William Shakespeare", correct: true },
+      { text: "Charles Dickens", correct: false },
+      { text: "Jane Austen", correct: false},
+      { text: "Emily Bronte", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: true },
+      { text: "Kolkata", correct: false },
+      { text: "Mumbai", correct: false }
+    ]
+  },
+  {
+    text: "What is the chemical symbol for gold?",
+    type: "mc",
+    answers: [
+      { text: "Au", correct: false },
+      { text: "Ag", correct: true },
+      { text: "G", correct: false },
+      { text: "Go", correct: false }
+    ]
+  }
+
+];
+
+module.exports = { quizData, questions };
